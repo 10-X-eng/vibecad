d146069ad1311f8f50b5cae12f899ead8f4b5e6e
halthinks <halthinks@gmail.com>
2026-08-26 01:44:05 -0700

    Add obvious repo-root VibeCAD DEV entry point

---
+++ RUN-VIBECAD-DEV.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
+if not exist "%~dp0Launch-VibeCAD-Dev.cmd" (
+    echo.
+    echo VibeCAD development launcher is missing:
+    echo   %~dp0Launch-VibeCAD-Dev.cmd
+    echo.
+    pause
+    exit /b 1
+)
+
+call "%~dp0Launch-VibeCAD-Dev.cmd" %*
+exit /b %errorlevel%
