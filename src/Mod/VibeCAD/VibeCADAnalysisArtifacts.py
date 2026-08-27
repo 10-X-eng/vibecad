1a24107a011cdba25416800380b1d7b5edbc1d06
halthinks <halthinks@gmail.com>
2026-08-26 21:21:14 -0700

    Add immutable Analysis artifact sealing

--- src/Mod/VibeCAD/VibeCADAnalysisArtifacts.py
+++ src/Mod/VibeCAD/VibeCADAnalysisArtifacts.py
@@ -9,9 +9,16 @@ from tool_impl.analysis_artifacts import (
     DEFAULT_MAXIMUM_FILES,
     FEM_COMPAT_DIGEST_ALGORITHM,
     STREAM_BLOCK_BYTES,
+    ARTIFACT_MANIFEST_VERSION,
     AnalysisArtifactError,
+    ArtifactDescriptor,
+    ArtifactManifest,
+    ContentAddressedArtifactStore,
     SealedDirectory,
+    seal_artifact,
     seal_directory,
+    validate_archive,
+    verify_artifact,
 )
 
 __all__ = (
@@ -19,7 +26,14 @@ __all__ = (
     "DEFAULT_MAXIMUM_FILES",
     "FEM_COMPAT_DIGEST_ALGORITHM",
     "STREAM_BLOCK_BYTES",
+    "ARTIFACT_MANIFEST_VERSION",
     "AnalysisArtifactError",
+    "ArtifactDescriptor",
+    "ArtifactManifest",
+    "ContentAddressedArtifactStore",
     "SealedDirectory",
+    "seal_artifact",
     "seal_directory",
+    "validate_archive",
+    "verify_artifact",
 )
